111a23bd58864d3215acd7e9ebf218b190b855fc
gpdno <gdeninno@suncoastde.com>
2016-02-01 20:19:55 -0500

    added class var

--- Times Play/HighscoreTableViewController.swift
+++ Times Play/HighscoreTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class HighscoreTableViewController: UITableViewController {
+    
+    var leaderboard = [Highscore]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
